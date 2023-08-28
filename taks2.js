d0f057f9588fc59b0fb39ddcdf29ddd43711bf60
taufikfuji <fujitaufik@gmail.com>
2023-08-28 22:04:38 +0700

    update taks2.js

--- taks2.js
+++ taks2.js
@@ -1,26 +1,31 @@
-function myName() {
-    let myName = "Fuji Taufik"
-    return myName
+// function myName() {
+//     let myName = "Fuji Taufik"
+//     return myName
+// }
+// console.log(myName());
+
+// /* ===========Length==================== */
+// function getNameLength(name) {
+//     return name.length;
+// }
+// let myName = "Fuji Taufik";
+// let length = getNameLength(myName);
+// console.log(length);
+
+
+// /*================Huruf Besar Semua ======================== */
+// let myName = " fuji taufik";
+// function capitalString(upper) {
+//     return upper.toUpperCase();
+// }
+// let capital = capitalString(myName);
+// console.log(capital);
+
+
+/*=================== Substring ===============================*/
+let myName = "fuji taufik";
+function subStr(name){
+    return name.substring(0,4);
 }
-console.log(myName());
-
-/* ===========Length==================== */
-function getNameLength(name) {
-    return name.length;
-}
-
-let myName = "Fuji Taufik";
-let length = getNameLength(myName);
-
-console.log(length);
-
-
-
-
-/*================Huruf Besar Semua ======================== */
-let myName = " fuji taufik";
-function capitalString(upper) {
-    return upper.toUpperCase();
-}
-let capital = capitalString(myName);
-console.log(capital);
+let subName = subStr(myName);
+console.log(subName);
