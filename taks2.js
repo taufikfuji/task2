2cfe112639723c09dc79eef6b4d25cc7391b8f5a
taufikfuji <fujitaufik@gmail.com>
2023-08-28 21:54:30 +0700

    merubah isi file taks2.js

---
+++ taks2.js
@@ -0,0 +1,26 @@
+function myName() {
+    let myName = "Fuji Taufik"
+    return myName
+}
+console.log(myName());
+
+===========Length==================== */
+function getNameLength(name) {
+    return name.length;
+}
+
+let myName = "Fuji Taufik";
+let length = getNameLength(myName);
+
+console.log(length);
+
+
+
+
+/*================Huruf Besar Semua ======================== */
+let myName = " fuji taufik";
+function capitalString(upper) {
+    return upper.toUpperCase();
+}
+let capital = capitalString(myName);
+console.log(capital);
