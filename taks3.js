14927bf8813f6afd31c17ff33db9e55cc89acd4b
taufikfuji <fujitaufik@gmail.com>
2023-08-30 20:20:40 +0700

    menambahkan file taks3.js

---
+++ taks3.js
@@ -0,0 +1,24 @@
+let a = 1;
+let b =2 ;
+
+function tambah(a, b) {
+    return a + b;
+  }
+  
+  function kurang(a, b) {
+    return a - b;
+  }
+  
+  function kali(a, b) {
+    return a * b;
+  }
+  
+  function bagi(a, b) {
+    return a / b;
+  }
+  
+  console.log("Penjumlahan:", tambah(a,b));
+  console.log("Pengurangan:", kurang(a,b));
+  console.log("Perkalian:", kali(a,b));
+  console.log("Pembagian:", bagi(a,b));
+  
