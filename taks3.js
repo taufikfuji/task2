beeb573823034d75a07dd6590e97a6dfc0c824d2
taufikfuji <fujitaufik@gmail.com>
2023-08-30 20:35:27 +0700

    update taks3.js

--- taks3.js
+++ taks3.js
@@ -22,3 +22,24 @@ function tambah(a, b) {
   console.log("Perkalian:", kali(a,b));
   console.log("Pembagian:", bagi(a,b));
   
+
+/*=============Function Declaration========*/
+let myName = "fuji taufik";
+
+function strToLength(inputName) {
+  let nameLength = inputName.length;
+  return nameLength;
+}
+
+const nameDeclaration = strToLength(myName);
+console.log(nameDeclaration); 
+
+/*=============Function Expression========*/
+let myName = "fuji taufik";
+const strToLengthExpression = function(inputName) {
+  let nameLength = inputName.length;
+  return nameLength;
+};
+
+const lengthExpression = strToLengthExpression(myName);
+console.log(lengthExpression); 
