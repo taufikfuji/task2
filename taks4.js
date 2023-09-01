6f80211418249d9b423979dbf5e50107cb7c73a0
taufikfuji <fujitaufik@gmail.com>
2023-09-01 21:11:23 +0700

    update taks4.js

--- taks4.js
+++ taks4.js
@@ -1,19 +1,30 @@
 
-let calculator = (input1,input2,operator)=> {
-    if (typeof input1 !== "number" || typeof input2 !=="number"){
-        return "invalid input, please input number";
-    }else if (operation=="+"){
-        return input1+input2;
-    }else if (operation=="-"){
-        return input1-input2;
-    }else if(operation=="*"){
-        return input1*input2;
-    }else if (operation=="/"){
-        return input1/input2;
-    }else{
-        return "error";
-    }
-}
+// let calculator = (input1,input2,operator)=> {
+//     if (typeof input1 !== "number" || typeof input2 !=="number"){
+//         return "invalid input, please input number";
+//     }else if (operation=="+"){
+//         return input1+input2;
+//     }else if (operation=="-"){
+//         return input1-input2;
+//     }else if(operation=="*"){
+//         return input1*input2;
+//     }else if (operation=="/"){
+//         return input1/input2;
+//     }else{
+//         return "error";
+//     }
+// }
 
-let result = calculator (2,5,"+");
-console.log(result);
+// let result = calculator (2,5,"+");
+// console.log(result);
+
+// let suuhuCelsius = 25;
+// function konversiCelsiusKeFahrenheit(celcius){
+//     let fahrenhiet = (celcius * 9/5) + 32;
+//     return fahrenhiet;
+// }
+// let suhuFahrenheit = konversiCelsiusKeFahrenheit(suuhuCelsius);
+// console.log(`Suhu ${suuhuCelsius} Celsius sama dengan ${suhuFahrenheit}`);
+
+let konversiCelsiusKeFahrenheit = (celcius) => fahrenhiet = (celcius * 9/5) + 32;
+console.log(konversiCelsiusKeFahrenheit(25));
